af8bfa5aa5cfd3af26af3fd87dabc36f6c0f0c60
gonmarmar5 <gonmarmar5@alum.us.es>
2022-01-14 10:53:06 +0100

    Ejercicio 1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,16 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +19,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Feeding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @NotNull
+    @Column(name = "start_date")
     LocalDate startDate;
+    
+    @NotNull
+    @Min(1)
+    @Column(name = "weeks_duration")
     double weeksDuration;
+
+    @ManyToOne
+    @NotNull
     Pet pet;   
 }
