5951ce6c6cfbe3a854e7e9deb101fe09f8808b35
gonmarmar5 <gonmarmar5@alum.us.es>
2022-01-14 11:19:22 +0100

    Ejercicio 3

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -20,6 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "feeding")
 public class Feeding {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,16 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +18,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "feeding_type")
 public class FeedingType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @NotNull
+    @Column(unique=true)
+    @Size(min=5,max=30)
     String name;
+
+    @NotNull
     String description;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name="pet_type_id")
     PetType petType;
 }
