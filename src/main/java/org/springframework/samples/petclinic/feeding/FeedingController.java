d7689ee651f1d2de835724f8ecbf7265ac6712ed
gonmarmar5 <gonmarmar5@alum.us.es>
2022-01-14 12:23:30 +0100

    Ejercicio10

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -5,11 +5,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.pet.Pet;
 import org.springframework.samples.petclinic.pet.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class FeedingController {
     
@@ -30,5 +35,31 @@ public class FeedingController {
 		model.put("feedingTypes", ft);
 		model.put("pets", p);
 		return "feedings/createOrUpdateFeedingForm";
+	}
+
+	@PostMapping(path = "/feeding/create")
+	public String processCreationForm(@Valid Feeding feeding,BindingResult result, Map<String, Object> model) {
+
+		if(result.hasErrors()){
+			Collection<FeedingType> ft = this.feedingService.getAllFeedingTypes();
+			Collection<Feeding> f = this.feedingService.getAll();
+			Collection<Pet> p = this.petService.getAllPets();
+
+			model.put("feeding", f);
+			model.put("feedingTypes", ft);
+			model.put("pets", p);
+			return "feedings/createOrUpdateFeedingForm";
+		}
+		else{
+			try{
+				feedingService.save(feeding);
+				model.put("message", "Feeding saved");
+			}
+			catch (UnfeasibleFeedingException ex){
+				result.reject("La mascota seleccionada no se le puede asignar el plan de alimentación especificado."); 
+			}
+		}
+
+		return "welcome";
 	}
 }
