ce21d4a60bc887671875d94c45b86f98653503a6
gonmarmar5 <gonmarmar5@alum.us.es>
2022-01-14 11:22:05 +0100

    Ejercicio 4

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
@@ -40,4 +41,8 @@ public class Feeding {
     @ManyToOne
     @NotNull
     Pet pet;   
+
+    @ManyToOne
+    @JoinColumn(name = "feeding_type")
+    FeedingType feedingType;
 }
