72c8d6fa70dccb90d875bbfb3830cf77ae9317f0
gonmarmar5 <gonmarmar5@alum.us.es>
2022-01-14 12:11:42 +0100

    Ejercicio 9

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,14 +1,34 @@
 package org.springframework.samples.petclinic.feeding;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class FeedingController {
     
-    @GetMapping({"/product/create"})
+	@Autowired
+	FeedingService feedingService;
+
+	@Autowired
+	PetService petService;
+
+    @GetMapping(path = "/feeding/create")
 	public String initCreationForm(Map<String, Object> model,Principal principal) {
+
+		Collection<FeedingType> ft = this.feedingService.getAllFeedingTypes();
+		Collection<Feeding> f = this.feedingService.getAll();
+		Collection<Pet> p = this.petService.getAllPets();
+
+		model.put("feeding", f);
+		model.put("feedingTypes", ft);
+		model.put("pets", p);
 		return "feedings/createOrUpdateFeedingForm";
 	}
 }
